237a9f05ecde1d109fda9ed903a885732faecc1d
RezaSh798 <rezakhorramisalarabadi@gmail.com>
2025-06-27 00:27:10 +0330

    Feature: Write controller

---
+++ ReservationService/API/Controllers/ReservationsController.cs
@@ -0,0 +1,53 @@
+using MediatR;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Reservation.Application.Commands;
+using Reservation.Application.Queries;
+using Reservation.Domain.Entities;
+
+namespace Reservation.API.Controllers;
+
+[ApiController]
+[Route("api/v1/[controller]")]
+public class ReservationsController(IMediator mediator) : ControllerBase
+{
+    private readonly IMediator _mediator = mediator;
+
+    [HttpGet]
+    public async Task<ActionResult<List<ReservationEntity>>> GetAll()
+    {
+        var reservations = await _mediator.Send(new GetAllReservationsQuery());
+        return Ok(reservations);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<ReservationEntity>> GetById(Guid id)
+    {
+        var reservation = await _mediator.Send(new GetReservationByIdQuery { Id = id });
+        if (reservation == null) return NotFound();
+        return Ok(reservation);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<ReservationEntity>> Create(CreateReservationCommand command)
+    {
+        var result = await _mediator.Send(command);
+        return CreatedAtAction(nameof(GetById), new { id = result.Id }, result);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<ActionResult<ReservationEntity>> Update(Guid id, UpdateReservationCommand command)
+    {
+        if (id != command.Id) return BadRequest();
+        var updated = await _mediator.Send(command);
+        if (updated == null) return NotFound();
+        return Ok(updated);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Delete(Guid id)
+    {
+        await _mediator.Send(new RemoveReservationCommand { Id = id });
+        return NoContent();
+    }
+}
